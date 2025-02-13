fda640d20799b51de0d6bc2cfb699767bec0df11
Vishesh Jha <166105063+Visheshjha11@users.noreply.github.com>
2025-02-13 15:23:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int num1, num2;
     char ch;
+    
+    // Taking input properly
     scanf("%d %d", &num1, &num2);
-    getchar(); 
+    getchar();  // Consume leftover newline character
     scanf("%c", &ch);
-    if(ch=='+'){
-        printf("%d", num1+num2);
-    }
-    else if(ch=='-'){
-        printf("%d", num1-num2);
-    }
-    else if(ch=='*'){
-        printf("%d", num1*num2);
-    }
-    else if(ch=='/'){
-        printf("%d", num1/num2);
-    }
-    else{
+
+    // Checking and performing operations
+    if (ch == '+') {
+        printf("%d", num1 + num2);
+    } 
+    else if (ch == '-') {
+        printf("%d", num1 - num2);
+    } 
+    else if (ch == '*') {
+        printf("%d", num1 * num2);
+    } 
+    else if (ch == '/') {
+        if (num2 == 0) {
+            printf("Error: Division by zero");
+        } else {
+            printf("%d", num1 / num2);
+        }
+    } 
+    else {
         printf("Invalid input");
     }
+    
     return 0;
-
 }
